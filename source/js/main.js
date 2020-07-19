2d6fc574bac03f0dc0d70593bb9e5515e0e4ca83
Юрий Ронин <yurkaronin@gmail.com>
2020-07-19 18:08:19 +0300

    Добавляю якорные ссылки + плавную прокрутку (скрипт в main.js)

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-  $('#nav-icon').click(function () {
-    $(this).toggleClass('open');
-  });
-});
+function slowScroll(id) {
+  var offset = 0;
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - offset
+  }, 1000);
+  return false;
+}
