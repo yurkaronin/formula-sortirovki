367d3ed920274dc3e11746e8049df4877f38be7d
Юрий Ронин <yurkaronin@gmail.com>
2020-07-19 21:19:02 +0300

    Добавляю верхний прогрессбар скрола страницы css + js

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,15 @@
+var progress = document.querySelector('.progress-bar');
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e) {
+  var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var result = windowScroll / windowHeight * 100;
+
+  progress.style.width = result + '%';
+}
+
 function slowScroll(id) {
   var offset = 0;
   $('html, body').animate({
